068b3623df2c120b0245f6b84e3120192c21d876
Ben Turner <exterior@benturner.com>
2013-12-17 00:32:51 -0500

    deleted some outdated files

--- linter/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail')
-)
