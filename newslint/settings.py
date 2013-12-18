9c947ee1d774f07ec5dee60e3b1e4291452973dd
Ben Turner <exterior@benturner.com>
2013-12-18 02:41:12 -0500

    allowed hosts for newslint

--- newslint/settings.py
+++ newslint/settings.py
@@ -47,7 +47,7 @@ else:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', u'ec2-54-197-17-235.compute-1.amazonaws.com', u'newslint.com', u'www.newslint.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', u'ec2-54-197-17-235.compute-1.amazonaws.com', u'newslint.com', u'www.newslint.com', '54.197.17.235']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
