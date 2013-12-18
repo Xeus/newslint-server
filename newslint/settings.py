5727286a2d0300bd190c5d09ad5fd2ee548b337f
Ben Turner <exterior@benturner.com>
2013-12-17 21:32:25 -0500

    copy all files to static

--- newslint/settings.py
+++ newslint/settings.py
@@ -32,7 +32,7 @@ if 'RDS_HOSTNAME' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'newslint.db',  # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': '',
