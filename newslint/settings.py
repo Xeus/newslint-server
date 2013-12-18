5f2ec554cf8acc19dfe5c5fe7103788431e1344e
Ben Turner <exterior@benturner.com>
2013-12-17 22:07:32 -0500

    db settings

--- newslint/settings.py
+++ newslint/settings.py
@@ -37,8 +37,8 @@ else:
             'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': '',  # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': '',
-            'PASSWORD': '',
+            'USER': vars.MYSQL_USER,
+            'PASSWORD': vars.MYSQL_PASS,
             'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
             'PORT': '',  # Set to empty string for default.
         }
